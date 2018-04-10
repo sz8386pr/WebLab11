a6261d18de97e5dada1a44757dec622b89fdbf29
sz8386pr <sz8386pr@gmail.com>
2018-04-10 18:25:40 -0500

    up to slide 55

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var apodService = require('../services/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('fetchpicture');
 });
+
+
+// Fetch a picture from NASA's APOD service
+router.get('/fetchpicture', function(req, res, next){
+  // if (req.query.picturetype === 'random') {
+  //   res.send('todo: get random picture');
+  // }
+  // else {
+  //   // default is today's Pictures
+  //   res.send('tody: get today\'s picture');
+  // }
+
+  // If random picture requested, fetch random picture
+  // Otherwise fecth today's picture
+
+  apodService(function(err, apod_data){
+    if (err){
+      res.render('apod_error', {message: err.message, title: 'Error'});
+    } else {
+      res.render('index', {apod: apod_data, title: 'APOD for $(apod_data.date_)'});
+    }
+  }, req.query.picturetype);
+})
 
 module.exports = router;
